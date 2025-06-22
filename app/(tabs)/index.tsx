16afea562699e948a1cb7758ff6060e893dc802a
Rodrigo Ortiz <rodrigo.ortiz@berkeley.edu>
2025-06-21 20:20:20 -0400

    feat: show trending movies and fixed updateSearchCount useEffect

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,8 +1,10 @@
 import MovieCard from "@/components/MovieCard";
 import SearchBar from "@/components/SearchBar";
+import TrendingCard from "@/components/TrendingCard";
 import { icons } from "@/constants/icons";
 import { images } from "@/constants/images";
 import { fetchMovies } from "@/services/api";
+import { getTrendingMovies } from "@/services/appwrite";
 import useFetch from "@/services/useFetch";
 import { useRouter } from "expo-router";
 import {
@@ -16,6 +18,13 @@ import {
 
 export default function Index() {
   const router = useRouter();
+
+  const {
+    data: trendingMovies,
+    loading: trendingLoading,
+    error: trendingError,
+  } = useFetch(getTrendingMovies);
+
   const {
     data: movies,
     loading: moviesLoading,
@@ -31,15 +40,15 @@ export default function Index() {
         contentContainerStyle={{ minHeight: "100%", paddingBottom: 10 }}
       >
         <Image source={icons.logo} className='w-12 h-10 mt-20 mb-5 mx-auto' />
-        {moviesLoading ? (
+        {moviesLoading || trendingLoading ? (
           <ActivityIndicator
             size='large'
             color='#0000ff'
             className='mt-10 self-center'
           />
-        ) : moviesError ? (
+        ) : moviesError || trendingError ? (
           <Text className='text-lg text-white font-bold mt-5 mb-3'>
-            Error: {moviesError?.message}
+            Error: {moviesError?.message || trendingError?.message}
           </Text>
         ) : (
           <View className='flex-1 mt-5'>
@@ -47,6 +56,27 @@ export default function Index() {
               onPress={() => router.push("/search")}
               placeholder='Search for a movie'
             />
+
+            {trendingMovies && (
+              <View className='mt-10'>
+                <Text className='text-lg text-white font-bold mb-3'>
+                  Trending Movies
+                </Text>
+              </View>
+            )}
+
+            <FlatList
+              horizontal
+              showsHorizontalScrollIndicator={false}
+              ItemSeparatorComponent={() => <View className='w-4' />}
+              className='mb-4 mt-3'
+              data={trendingMovies}
+              renderItem={({ item, index }) => (
+                <TrendingCard movie={item} index={index}/>
+              )}
+              keyExtractor={(item) => item.movie_id.toString()}
+            />
+
             <Text className='text-lg text-white font-bold mt-5 mb-3'>
               Latest Movies
             </Text>
--- services/appwrite.ts
+++ services/appwrite.ts
@@ -33,7 +33,7 @@ export const updateSearchCount = async (query: string, movie: Movie) => {
         movie_id: movie.id,
         count: 1,
         title: movie.title,
-        poster_url: "https://image.tmdb.org/t/p/w500${movir.poster_path}",
+        poster_url: `https://image.tmdb.org/t/p/w500${movie.poster_path}`,
       });
     }
   } catch (error) {
@@ -43,4 +43,20 @@ export const updateSearchCount = async (query: string, movie: Movie) => {
   // check if a record of that search has already been stored
   // if a document is found, increment the searchCount field
   // if no document is found, create a new document in Appwrite database -> count: 1
+};
+
+export const getTrendingMovies = async (): Promise<
+  TrendingMovie[] | undefined
+> => {
+  try {
+    const result = await database.listDocuments(DATABASE_ID, COLLECTION_ID, [
+      Query.limit(5),
+      Query.orderDesc("count"),
+    ]);
+
+    return result.documents as unknown as TrendingMovie[];
+  } catch (error) {
+    console.log(error);
+    return undefined;
+  }
 };
